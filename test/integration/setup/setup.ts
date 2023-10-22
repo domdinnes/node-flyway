57e9b05b29d6dd35689ccd9a1c3b929a08e5cc7d
Dominic Dinnes <dom.dinnes@gmail.com>
2023-10-22 14:11:25 +0100

    Changes

--- test/integration/setup/setup.ts
+++ test/integration/setup/setup.ts
@@ -75,8 +75,8 @@ const pollDatabaseForLiveness = async (
     maxAttempts: number,
     waitInterval: number
 ) => {
-    const checkDatabaseLiveness = `psql --host=localhost --port=${port} --username=postgres --dbname=postgres --command="\\d"`;
-    //const checkDatabaseLiveness = `psql postgresql://postgres:${password}@localhost:${port} -c "\\d"`;
+    //const checkDatabaseLiveness = `psql --host=localhost --port=${port} --username=postgres --dbname=postgres --command="\\d"`;
+    const checkDatabaseLiveness = `psql postgresql://postgres:${password}@localhost:${port} -c "\\d"`;
 
     for (let i = 0; i < maxAttempts; i++) {
 
